9ca4fed35b24055b6f4c81022e4f16c524a6f1a4
Logan Losee <logan.losee99@gmail.com>
2024-02-18 18:32:59 -0700

    add post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -63,18 +63,17 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", authenticationMiddleware, (req, res) => {
-  // create a new post
-  Post.create({
-    title: req.body.title,
-    content: req.body.content,
-    user_id: req.session.user_id,
-  })
+    Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
-});
+  });
 
 router.put("/:id", authenticationMiddleware, (req, res) => {
   Post.update(
--- public/js/add-post.js
+++ public/js/add-post.js
@@ -2,24 +2,17 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('input[name="content"]').value;
+    const content = document.querySelector('textarea[name="content"]').value;
   
     const response = await fetch(`/api/posts`, {
-      method: "POST",
-      body: JSON.stringify({
-        title,
-        content,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+        method: "POST",
+        body: JSON.stringify({ title, content }),
+        headers: { "Content-Type": "application/json" },
+      });
   
     response.ok
       ? document.location.replace("/dashboard")
       : alert(response.statusText);
-  };
+};
   
-  document
-    .querySelector("#new-post-form")
-    .addEventListener("submit", newFormHandler);
+document.querySelector("#new-post-form").addEventListener("submit", newFormHandler);
