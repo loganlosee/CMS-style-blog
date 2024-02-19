9d9f24111981c909bee585bfef478535bc09c1b4
Logan Losee <logan.losee99@gmail.com>
2024-02-18 19:20:09 -0700

    bug fixes

---
+++ public/js/logout.js
@@ -0,0 +1,10 @@
+const logout = async () => {
+    const response = await fetch("/api/users/logout", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+    });
+  
+    response.ok ? document.location.replace("/") : alert(response.statusText);
+  };
+  
+  document.querySelector("#logout").addEventListener("click", logout);
