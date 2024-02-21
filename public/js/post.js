3c2d152c5ca800420ea0a939463d62b686440fc3
Logan Losee <logan.losee99@gmail.com>
2024-02-20 21:06:09 -0700

    handlebars fixes and readMe written

--- public/js/post.js
+++ public/js/post.js
@@ -37,3 +37,29 @@ const newCommentHandler = async (event) => {
 };
 
 document.querySelector('#newCommentButton').addEventListener('click', newCommentHandler);
+
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="content"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    response.ok
+      ? document.location.replace("/homepage")
+      : alert(response.statusText);
+  };
+  
+  document
+    .querySelector("#new-post-form")
+    .addEventListener("submit", newFormHandler);
